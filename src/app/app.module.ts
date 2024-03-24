b2fdf0a456463d06f19dbf2d9f7c4ad7de4c8f88
Iulian Tobica <iulian1997@hotmail.it>
2024-03-24 15:35:56 +0100

    1.5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { ProdottiComponent } from './prodotti/prodotti.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
     ProdottiComponent,
   ],
   imports: [
@@ -15,6 +13,6 @@ import { ProdottiComponent } from './prodotti/prodotti.component';
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent, ProdottiComponent]
+  bootstrap: [ProdottiComponent]
 })
 export class AppModule { }
